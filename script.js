9e7689e171334cd084532f20c2a73006d1733830
hasanerol1 <hasanerol89@gmail.com>
2024-10-31 03:01:52 +0300

    hatalar giderildi

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
 const arrows = document.querySelectorAll(".arrow");
 const movieLists = document.querySelectorAll(".movie-list");
 
-arrows.forEach((arrow, i) => {
-  const withRatio = Math.floor(window.innerWidth / 300) 
+arrows.forEach((arrow, index) => {
   let clickCounter = 0;
-   const imageItem =  movieLists[i].querySelectorAll("img").length;
-   arrow.addEventListener("click", function () {
-    clickCounter++;
-   if (imageItem - (4 + clickCounter) + (4 - withRatio) >= 0) {
-     movieLists[i].style.transform = `translateX(${
-      movieLists[i].computedStyleMap().get("transform")[0].xvalue - 300 }px)`;
-    } else {
-      movieLists[i].style.transform = "translateX(0)";
-   clickCounter = 0;
-    }
 
-   });
+  arrow.addEventListener("click", () => {
+    const imageItemCount = movieLists[index].querySelectorAll("img").length;
+    const itemWidth = 300; 
+    const totalWidth = itemWidth * imageItemCount;
+
+  
+    const maxClicks =
+      Math.ceil(totalWidth / itemWidth) -
+      Math.floor(window.innerWidth / itemWidth);
+
+    if (clickCounter < maxClicks) {
+      clickCounter++;
+      movieLists[index].style.transform = `translateX(-${
+        itemWidth * clickCounter
+      }px)`;
+    } else {
+      clickCounter = 0;
+      movieLists[index].style.transform = `translateX(0)`;
+    }
+  });
 });
 
 /* dark mode */
-const ball = document.querySelector(".toggle-ball");
-const items = document.querySelectorAll(".container,.navbar,.sidebar,.sidebar i,.toggle,.toggle-ball,.movie-list-filter select,.movie-list-title")
-ball.addEventListener("click", function(){
-   items.forEach((item)=> item.classList.toggle("active"));
 
-})
+const ball = document.querySelector(".toggle-ball");
+const items = document.querySelectorAll(
+  ".container,.navbar,.sidebar,.sidebar i,.toggle,.toggle-ball,.movie-list-filter select,.movie-list-title"
+);
+
+ball.addEventListener("click", function () {
+  items.forEach((item) => item.classList.toggle("active"));
+});
